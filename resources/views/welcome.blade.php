7cd7281347b40664418e95acf1534112d260e6d2
M <coopsmwc@googlemail.com>
2019-04-15 16:09:05 +0100

    Added docker files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 2
+                    Laravel API
                 </div>
 
                 <div class="links">
